e3fbb76fd2eaaadefb1c155640c7d7d3f217272f
NivanV <nivan.dsa@capillarytech.com>
2023-04-05 15:30:58 +0530

    adds getMeetings by filters

--- src/main/java/com/example/calender/service/MeetingService.java
+++ src/main/java/com/example/calender/service/MeetingService.java
@@ -3,8 +3,11 @@ package com.example.calender.service;
 import com.example.calender.constants.MeetingStatus;
 import com.example.calender.entity.Meeting;
 import com.example.calender.exception.PolicyViolationException;
+import org.aspectj.apache.bcel.classfile.Module;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface MeetingService {
@@ -16,6 +19,12 @@ public interface MeetingService {
     public MeetingStatus changeMeetingStatus(Long id, MeetingStatus newStatus);
 
     public Meeting getMeetingDetails(Long id);
+
+    public List<Meeting> getMeetingsInCustomRange(Date start, Date end);
+
+    public List<Meeting> getMeetingsInParticularWeek(char sign,int byWeek);
+
+    public List<Meeting> getParticularEmployeeMeetings(List<Meeting> meetingsList, Long id);
 
 
 }
